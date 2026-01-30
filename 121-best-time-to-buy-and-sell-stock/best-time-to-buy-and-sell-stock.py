953695540c3518acf81a333c988ec5f49e58750c
Luke Muehring <muehring.luke@gmail.com>
2026-01-30 09:28:04 -0700

    Time: 67 ms (52.55%) | Memory: 28.7 MB (30.66%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -10,7 +10,7 @@ class Solution:
 
         # while r is within the array,
         while r < len(prices):
-            # check if r.value < l.value and r > l (index check - you must buy from a day in the future)
+            # check if r.value < l.value
             if prices[r] < prices[l]:
                 # set l = r, and move r to next index
                 l = r
