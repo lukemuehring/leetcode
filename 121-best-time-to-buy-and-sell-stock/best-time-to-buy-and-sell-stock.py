87642658516dd05e4a966b815197dc8ccee5a998
Luke Muehring <muehring.luke@gmail.com>
2026-01-30 09:27:15 -0700

    Time: 55 ms (63.11%) | Memory: 28.7 MB (30.66%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,40 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if (len(prices) < 2):
-            return 0 
-
-        # # loop through the array and keep track of the current min (buy) and max (sell), store their index
-        # minP, maxP = 0, 0
-        # profit = 0
-        # for i in range(1, len(prices)):
-        #     if prices[i] < prices[minP]:
-        #         # if (prices[maxP] - prices[minP] > profit):
-        #         #     profit = prices[maxP] - prices[minP]
-        #         minP = i
-        #         maxP = i
-            
-        #     if prices[i] > prices[maxP]:
-        #         maxP = i
-        #         if (prices[maxP] - prices[minP]) > profit:
-        #             profit = prices[maxP] - prices[minP]
-
-        # return profit
-
-        # Neetcode
-
-        l, r = 0, 1
         profit = 0
-        while r < len(prices):
-            if prices[l] > prices[r]:
-                l=r
-                r+=1
-                continue
-            currProfit = prices[r] - prices[l]
-            if currProfit > profit:
-                profit = currProfit
-            r+=1
+        if len(prices) < 2:
+            return profit
+    
+        # two pointers, left (buy) and right (sell)
+        l = 0
+        r = 1
 
+        # while r is within the array,
+        while r < len(prices):
+            # check if r.value < l.value and r > l (index check - you must buy from a day in the future)
+            if prices[r] < prices[l]:
+                # set l = r, and move r to next index
+                l = r
+                r += 1
+                continue
+            # if this purchase is more profitable, save it
+            if prices[r] - prices[l] > profit:
+                profit = prices[r] - prices[l]
+            r += 1
+        
         return profit
+        
             
             
     
