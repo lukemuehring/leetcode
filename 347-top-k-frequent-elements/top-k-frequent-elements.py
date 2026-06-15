b9c055d03d9b5b58e86cbb3f1e68c53451492dcb
Luke Muehring <muehring.luke@gmail.com>
2026-06-15 13:58:51 -0600

    Time: 4 ms (93.43%) | Memory: 14.3 MB (57.23%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,35 +1,26 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashM = {}
-        freq = [[] for i in range(len(nums)+1)]
+import heapq
 
-        for n in nums:
-            if n in hashM:
-                hashM[n] += 1
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        maxHeap = []
+        countSet = {}
+        for num in nums:
+            if num not in countSet:
+                countSet[num] = 1
             else:
-                hashM[n] = 1
+                countSet[num] += 1
         
-        for num, count in hashM.items():
-            freq[count].append(num)
+        for key in countSet:
+            heapq.heappush(maxHeap, (-countSet[key], key))
         
-        results = []
+        result = []
+        for n in range(k):
+            result.append((heapq.heappop(maxHeap)[1]))
 
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                results.append(n)
-                if len(results) == k:
-                    return results
-
-        # (my boof ass code to iterate through the top k elements)
-        # i = len(freq) - 1
-        # while k > 0:
-        #     currNums = freq[i]
-        #     if len(currNums) > 0:
-        #         for j in range(len(currNums)):
-        #             if k > 0:
-        #                 results.append(currNums[j])
-        #                 k -= 1
-        #             else:
-        #                 break
-        #     i -= 1
-        return results
+        return result
+        
